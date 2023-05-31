45a795e8090894094b324eb28b0eff51e6c26d3d
MrJ-2k04 <j.soni2k04@gmail.com>
2023-05-31 13:56:49 +0530

    Added Route Guard

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -1,20 +1,35 @@
 
-import { Route, Routes as Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, useRoutes } from "react-router-dom";
 import { CreateBlogPage, HomePage, LoginPage, RegistrationPage } from "./Pages";
 import NotFound from "./Pages/NotFound";
 import { ROUTE_CREATE, ROUTE_HOME, ROUTE_LOGIN, ROUTE_NOT_FOUND, ROUTE_REGISTER } from "./store/constants";
 
+
 function Routes() {
-    return (
-        <Switch>
-            <Route element={<HomePage />} path={ROUTE_HOME} />
-            <Route element={<LoginPage />} path={ROUTE_LOGIN} />
-            <Route element={<RegistrationPage />} path={ROUTE_REGISTER} />
-            <Route element={<CreateBlogPage />} path={ROUTE_CREATE} />
-            {/* <Route element={<SingleBlog />} path={ROUTE_BLOG} /> */}
-            <Route element={<NotFound />} path={ROUTE_NOT_FOUND} />
-        </Switch>
-    );
+    const user = useSelector(state => state.auth.user);
+    return useRoutes([
+        {
+            path: ROUTE_HOME,
+            element: <HomePage />,
+        },
+        {
+            path: ROUTE_LOGIN,
+            element: <LoginPage />,
+        },
+        {
+            path: ROUTE_REGISTER,
+            element: <RegistrationPage />,
+        },
+        {
+            path: ROUTE_CREATE,
+            element: user ? <CreateBlogPage /> : <Navigate to={ROUTE_LOGIN} replace />,
+        },
+        {
+            path: ROUTE_NOT_FOUND,
+            element: <NotFound />,
+        },
+    ]);
 }
 
 export default Routes;
